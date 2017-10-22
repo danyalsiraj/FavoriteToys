3e819458a1ebe6f4e5c623abca6676dbf737a82b
danyalsiraj <danyal61@gmail.com>
2017-10-21 23:57:35 -0400

    added a list of toys and scroll view and changed to frame layout

--- app/src/main/java/com/example/danyal/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/danyal/favoritetoys/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.danyal.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare a textview variable called ToyList
+    TextView ToyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToyListView=(TextView) findViewById(R.id.toyList);
+        // use find by view to get a refrence to textview from layout
+
+        //use the static toybox.gettoynames method to store the names in a string
+        String[] toyName=ToyBox.getToyNames();
+        //loop through each toy and append the name in textview
+        for (String toy:toyName){
+            ToyListView.append(toy+"\n\n\n");
+        }
+
+
     }
 }
